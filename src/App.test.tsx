c65e7940ad49e003419e619b01d204649b654991
Christian Shinkle <cshinkle2018@gmail.com>
2021-03-26 23:51:14 -0500

    added assets, components, utils

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Layout', () => {
+  it('should render learn react link', () => {
+    render(<App />);
+    const linkComponent = screen.getByRole('link');
+    expect(linkComponent).toHaveTextContent('Learn React');
+  });
 });
