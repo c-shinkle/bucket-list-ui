4cc861985b4d3b2df2be9f6b1d47f4d5325f32c2
Christian Shinkle <cshinkle2018@gmail.com>
2021-05-03 20:45:02 -0500

    oops, honey I formatted the kids

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,7 @@
-import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
-import * as s3Deploy from '@aws-cdk/aws-s3-deployment';
-import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Deploy from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,25 +11,29 @@ export class CdkStack extends cdk.Stack {
     const bucket = new s3.Bucket(this, "CreateReactAppBucket", {
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      websiteIndexDocument: "index.html"
+      websiteIndexDocument: "index.html",
     });
 
     // Deployment
     const src = new s3Deploy.BucketDeployment(this, "DeployCRA", {
       sources: [s3Deploy.Source.asset("../build")],
-      destinationBucket: bucket
+      destinationBucket: bucket,
     });
 
     // Cloudfront
-    const cf = new cloudfront.CloudFrontWebDistribution(this, "CDKCRAStaticDistribution", {
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: bucket
+    const cf = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "CDKCRAStaticDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
           },
-          behaviors: [{ isDefaultBehavior: true }]
-        },
-      ]
-    });
+        ],
+      }
+    );
   }
 }
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-describe('App Layout', () => {
-
-  it('should render memo image', () => {
+describe("App Layout", () => {
+  it("should render memo image", () => {
     render(<App />);
 
-    const memo = screen.queryByRole('img');
+    const memo = screen.queryByRole("img");
 
     expect(memo).toBeInTheDocument();
   });
 
-  it('should render under construction text', () => {
+  it("should render under construction text", () => {
     render(<App />);
-    
-    screen.getByText('My Bucket List is under construction. Come back soon!');
+
+    screen.getByText("My Bucket List is under construction. Come back soon!");
   });
 });
